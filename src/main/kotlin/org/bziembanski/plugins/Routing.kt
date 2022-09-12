0023609a4021ce1d7b9fb079aec436505299e5fc
bziembanski <bziembanski@gmail.com>
2022-09-12 15:58:31 +0200

    removed class usage for toJpg functionality

--- src/main/kotlin/org/bziembanski/plugins/Routing.kt
+++ src/main/kotlin/org/bziembanski/plugins/Routing.kt
@@ -3,7 +3,6 @@ package org.bziembanski.plugins
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
 import io.ktor.server.routing.*
-import org.bziembanski.toJPG.ToJpgService
 import org.bziembanski.toJPG.toJpg
 
 fun Application.configureRouting() {
@@ -11,6 +10,6 @@ fun Application.configureRouting() {
 
     routing {
         resource("/", "index.html", "static")
-        toJpg(ToJpgService())
+        toJpg()
     }
 }
--- src/main/kotlin/org/bziembanski/toJPG/Routing.kt
+++ src/main/kotlin/org/bziembanski/toJPG/Routing.kt
@@ -8,69 +8,170 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import org.apache.pdfbox.Loader
+import org.apache.pdfbox.io.MemoryUsageSetting
+import org.apache.pdfbox.pdmodel.PDDocument
+import org.apache.pdfbox.pdmodel.PDPage
+import org.apache.pdfbox.pdmodel.PDPageContentStream
+import org.apache.pdfbox.pdmodel.common.PDRectangle
+import org.apache.pdfbox.pdmodel.graphics.image.JPEGFactory
+import org.apache.pdfbox.rendering.ImageType
+import org.apache.pdfbox.rendering.PDFRenderer
+import org.apache.pdfbox.tools.imageio.ImageIOUtil
 import java.io.DataOutputStream
 import java.io.File
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
 import java.nio.file.StandardOpenOption
+import javax.imageio.ImageIO
 
-fun Route.toJpg(service: ToJpgService) {
+fun Route.toJpg() {
+    val dpi = 300f //resolution of JPGs
+    val ppi = 72f //standard PDF PPI
+
+    val imageExtension = "jpg"
+    val pdfExtension = "pdf"
+
+    val uploadsDir = "uploads"
+
+    val newFileName = "-readOnly"
+
+    fun Int.getPDSize(): Float {
+        return (this.toFloat() / dpi) * ppi
+    }
+
+    suspend fun saveRequestFile(fileBytes: ByteArray, path: Path) {
+        withContext(Dispatchers.IO) {
+            Files.createDirectories(path.parent)
+            DataOutputStream(
+                Files.newOutputStream(
+                    path,
+                    StandardOpenOption.CREATE,
+                    StandardOpenOption.WRITE
+                )
+            ).apply {
+                write(fileBytes)
+                flush()
+                close()
+            }
+        }
+    }
+
+    suspend fun pdfToImages(fileName: String): List<String> {
+        return withContext(Dispatchers.IO) {
+            val document = Loader.loadPDF(
+                Files.newInputStream(
+                    Paths.get(
+                        uploadsDir,
+                        fileName
+                    )
+                ),
+                MemoryUsageSetting.setupTempFileOnly()
+            )
+            val pdfRenderer = PDFRenderer(document).apply {
+                isSubsamplingAllowed = true
+            }
+            val imagesList = mutableListOf<String>()
+
+            for (i in 0 until document.numberOfPages) {
+                try {
+                    val bim = pdfRenderer.renderImageWithDPI(i, dpi, ImageType.RGB)
+                    val path = Paths.get(uploadsDir, "$fileName$i.$imageExtension")
+                    ImageIOUtil.writeImage(bim, path.toString(), dpi.toInt())
+                    imagesList.add(path.toString())
+
+                } catch (e: Error) {
+                    e.printStackTrace()
+                }
+
+
+            }
+            document.close()
+            imagesList
+        }
+    }
+
+    suspend fun imagesToPdf(fileName: String, list: List<String>): String {
+        return withContext(Dispatchers.IO) {
+            val document = PDDocument()
+            list.forEach { path ->
+                val image = ImageIO.read(File(path))
+
+                val rectangle = PDRectangle(
+                    image.width.getPDSize(),
+                    image.height.getPDSize()
+                )
+                val page = PDPage(rectangle)
+
+                val pdImageXObject = JPEGFactory.createFromImage(document, image)
+                PDRectangle()
+                PDPageContentStream(document, page).apply {
+                    drawImage(
+                        pdImageXObject,
+                        0f,
+                        0f,
+                        rectangle.width,
+                        rectangle.height,
+                    )
+                    close()
+                }
+                document.addPage(page)
+            }
+            val path = Paths.get(uploadsDir, "$fileName$newFileName.$pdfExtension")
+            document.save(path.toString())
+            document.close()
+            path.toString()
+        }
+
+    }
+
     route("/toJpg") {
         post {
-            lateinit var fileName: String
-            lateinit var path: Path
-            lateinit var fileBytes: ByteArray
             val filesToDelete = mutableListOf<String>()
             call.receiveMultipart().forEachPart { part ->
                 when (part) {
                     is PartData.FileItem -> {
-                        fileName = part.originalFileName ?: ""
-                        fileBytes = part.streamProvider().readBytes()
-                        path = Paths.get(ToJpgService.uploadsDir, fileName)
+                        val fileName = part.originalFileName ?: ""
+                        val fileBytes = part.streamProvider().readBytes()
+                        val path = Paths.get(uploadsDir, fileName)
+
+                        saveRequestFile(fileBytes, path)
+                        filesToDelete.add(path.toString())
+
+                        pdfToImages(fileName).also { list ->
+                            filesToDelete.addAll(list)
+
+                            if (list.isNotEmpty()) {
+                                val name = imagesToPdf(fileName, list)
+                                filesToDelete.add(name)
+
+                                val image = File(name)
+                                call.response.header(
+                                    HttpHeaders.ContentDisposition,
+                                    ContentDisposition.Attachment.withParameter(
+                                        ContentDisposition.Parameters.FileName,
+                                        name
+                                    )
+                                        .toString()
+                                )
+                                call.respondFile(image)
+                            } else {
+                                call.respondText(
+                                    text = "Błąd serwera",
+                                    status = HttpStatusCode.InternalServerError
+                                )
+                            }
+                        }
                     }
 
                     else -> {}
                 }
             }
             withContext(Dispatchers.IO) {
-                Files.createDirectories(path.parent)
-                DataOutputStream(
-                    Files.newOutputStream(
-                        path,
-                        StandardOpenOption.CREATE,
-                        StandardOpenOption.WRITE
-                    )
-                ).apply {
-                    write(fileBytes)
-                    flush()
-                    close()
+                filesToDelete.forEach {
+                    File(it).delete()
                 }
-            }
-            filesToDelete.add(path.toString())
-            val list = service.pdfToImages(fileName).also{
-                filesToDelete.addAll(it)
-            }
-            if (list.isNotEmpty()) {
-                val name = service.imagesToPdf(fileName, list)
-                filesToDelete.add(name)
-                val image = File(name)
-                call.response.header(
-                    HttpHeaders.ContentDisposition,
-                    ContentDisposition.Attachment.withParameter(
-                        ContentDisposition.Parameters.FileName,
-                        name
-                    )
-                        .toString()
-                )
-                call.respondFile(image)
-                withContext(Dispatchers.IO){
-                    filesToDelete.forEach {
-                        File(it).delete()
-                    }
-                }
-            } else {
-                call.respondText(text = "Błąd serwera", status = HttpStatusCode.InternalServerError)
             }
         }
     }
